fc90869c73a2fca0554e6d15e395498e7bad75d7
TalCohenHarari <74271133+TalCohenHarari@users.noreply.github.com>
2021-01-04 14:37:20 +0200

    Add files via upload
    For the DataBase

---
+++ dbo.Category.data.sql
@@ -0,0 +1,8 @@
+SET IDENTITY_INSERT [dbo].[Category] ON
+INSERT INTO [dbo].[Category] ([CategoryID], [CategoryName]) VALUES (1, N'ביגוד והנעלה')
+INSERT INTO [dbo].[Category] ([CategoryID], [CategoryName]) VALUES (2, N'ביטוח')
+INSERT INTO [dbo].[Category] ([CategoryID], [CategoryName]) VALUES (3, N'חשמל')
+INSERT INTO [dbo].[Category] ([CategoryID], [CategoryName]) VALUES (4, N'דלק')
+INSERT INTO [dbo].[Category] ([CategoryID], [CategoryName]) VALUES (5, N'מזון')
+INSERT INTO [dbo].[Category] ([CategoryID], [CategoryName]) VALUES (6, N'שונות')
+SET IDENTITY_INSERT [dbo].[Category] OFF
---
+++ dbo.GiftCard.data.sql
@@ -0,0 +1,8 @@
+SET IDENTITY_INSERT [dbo].[GiftCard] ON
+INSERT INTO [dbo].[GiftCard] ([GiftCardID], [GiftCardName], [ExpireDate], [Price]) VALUES (1, N'DreamCard', N'0001-01-01 00:00:00', 50)
+INSERT INTO [dbo].[GiftCard] ([GiftCardID], [GiftCardName], [ExpireDate], [Price]) VALUES (2, N'DreamCard', N'0001-01-01 00:00:00', 100)
+INSERT INTO [dbo].[GiftCard] ([GiftCardID], [GiftCardName], [ExpireDate], [Price]) VALUES (3, N'DreamCard', N'0001-01-01 00:00:00', 200)
+INSERT INTO [dbo].[GiftCard] ([GiftCardID], [GiftCardName], [ExpireDate], [Price]) VALUES (4, N'BuyMe', N'0001-01-01 00:00:00', 50)
+INSERT INTO [dbo].[GiftCard] ([GiftCardID], [GiftCardName], [ExpireDate], [Price]) VALUES (5, N'BuyMe', N'0001-01-01 00:00:00', 100)
+INSERT INTO [dbo].[GiftCard] ([GiftCardID], [GiftCardName], [ExpireDate], [Price]) VALUES (6, N'BuyMe', N'0001-01-01 00:00:00', 200)
+SET IDENTITY_INSERT [dbo].[GiftCard] OFF
